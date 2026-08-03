fdc25f22df768eec17386fadb31a04ab39b96363
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-08-03 13:14:00 +0530

    feat: new commit

---
+++ RAG/index.js
@@ -0,0 +1,29 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+import { OpenAIEmbeddings } from "@langchain/openai"
+import { QdrantVectorStore } from "@langchain/qdrant"
+import { env } from "../env.ts"
+import path from "path"
+
+async function generateEmbeddings(filepath) {
+    const loader = new PDFLoader(filepath);
+    const document = await loader.load();
+
+    const embeddings = new OpenAIEmbeddings({
+        configuration: {
+            baseURL: "https://openrouter.ai/api/v1"
+        },
+        model: "text-embedding-3-small",
+        apiKey: env.OPENROUTER_API_KEY,
+    })
+
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings,{
+      url: 'http://localhost:6333',
+      collectionName: 'dsa-docs',
+    })
+
+    await vectorStore.addDocuments(document)
+    console.log("Documents indexed successfully")
+}
+
+generateEmbeddings(path.join(import.meta.dirname, "hp.pdf"))
+
