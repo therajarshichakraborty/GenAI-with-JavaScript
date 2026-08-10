6a3b779276131233403ed3e985ec867e5af4883f
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-08-10 17:30:27 +0530

    chore: add text splitter

---
+++ RAG/text-query.js
@@ -0,0 +1,70 @@
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { QdrantVectorStore } from '@langchain/qdrant';
+import OpenAI from 'openai';
+import { env } from '../env.ts';
+
+const client = new OpenAI({
+    baseURL: "https://openrouter.ai/api/v1",
+    apiKey:""
+});
+
+async function query(userQuery) {
+    // Convert user query to vector embeddings?
+    // Initalize the embedding model
+    const embeddings = new OpenAIEmbeddings({
+        configuration: {
+            baseURL: "https://openrouter.ai/api/v1"
+        },
+        model: 'text-embedding-3-small',
+        apiKey:""
+      }
+    )
+    console.log("embedding created");
+    
+    // search the vectors in the qdrant
+    // The vector store
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(
+        embeddings, // Use this embedding model
+        {
+            url: 'http://localhost:6333',
+            collectionName: 'ai-generated-poem',
+        },
+    );
+
+    // get simialr vectors and chunks?
+    const vectorRetriver = vectorStore.asRetriever({ k: 5 });
+    const results = await vectorRetriver.invoke(userQuery);
+
+    // feed those chunks to llm model and do a simple chat with {userQuery}
+    const context = results
+        .map(
+            (e) =>
+                `Book/Source: ${e.metadata?.source || 'Unknown'}\nPage: ${e.metadata?.loc?.pageNumber ?? 'N/A'}\nContent:\n${e.pageContent}`
+        )
+        .join('\n\n---\n\n');
+
+    const SYSTEM_PROMPT = `
+    You are an expert in answering user queries based on the provided context about documents.
+    Do not answer anything beyond what is provided.
+
+    Always answer the user concisely and tell on which page number (if available) that content is located and the name/source of the book.
+
+    User Documents Context:
+    ${context}
+  `;
+
+    const llmResponse = await client.chat.completions.create({
+        model: 'gpt-4o',
+        max_tokens: 500,
+        messages: [
+            { role: 'system', content: SYSTEM_PROMPT },
+            { role: 'user', content: userQuery },
+        ],
+    });
+
+    console.log(`LLM Response:`, llmResponse.choices[0].message.content);
+    console.log(SYSTEM_PROMPT);
+
+}
+
+query('summarize the poem');
